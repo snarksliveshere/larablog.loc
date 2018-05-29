b68c6a280da7cfcced353d3453daa5bb2e64328c
snarksliveshere <snarksliveshere@gmail.com>
2018-05-29 21:51:08 +0300

    start larablog frontend

---
+++ app/Post.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Support\Facades\Storage;
+
+class Post extends Model
+{
+
+    use Sluggable;
+
+    const IS_DRAFT = 0;
+    const  IS_PUBLIC = 1;
+
+    protected $fillable = ['title', 'content', 'date'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+//        теперь я смогу делать такую штуку
+//        $post = Post::find(1);
+//        $post->category()->title - связь с категорией по hasOne
+
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(
+            Tag::class,
+            'post_tags', // таблица
+            'post_id', // id этой (пост) модели
+            'tag_id' // id таблицы, с которой связываюсь - тэги $post->tags - все тэги
+        );
+    }
+
+    // translit! автоматический
+    public function sluggable()
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
+
+    public static function add($fields)
+    {
+        $post = new static;
+        $post->fill($fields);
+        // TODO: заглушка. поменять
+        $post->user_id = 1;
+        $post->save();
+
+        return $post;
+    }
+
+    public function edit($fields)
+    {
+        $this->fill($fields);
+        $this->save();
+    }
+
+    public function remove()
+    {
+        // удалить сопутствующие связи
+        $this->removeImage();
+        $this->delete();
+    }
+
+    public function uploadImage($image)
+    {
+        if ($image == null) { return; }
+        $this->removeImage();
+        $filename = str_random(10) . '.' . $image->extension();
+        $image->storeAs('uploads', $filename);
+        $this->image = $filename;
+        $this->save();
+    }
+
+    public function removeImage()
+    {
+        if ($this->image != null) {
+            Storage::delete('uploads/' . $this->image);
+        }
+    }
+    public function getImage()
+    {
+        if ($this->image == null) {
+            return '/img/no-image.png';
+        }
+
+        return '/uploads/' . $this->image;
+    }
+
+    public function setCategory($id)
+    {
+        if ($id == null) { return; }
+
+        $this->category_id = $id;
+        $this->save();
+    }
+
+    public function setTags($ids)
+    {
+        if ($ids == null) { return; }
+
+        $this->tags()->sync($ids);
+    }
+
+    public function setDraft()
+    {
+        $this->status = Post::IS_DRAFT;
+        $this->save();
+    }
+
+    public function setPublic()
+    {
+        $this->status = Post::IS_PUBLIC;
+        $this->save();
+    }
+
+    public function toggleStatus($value)
+    {
+        if ($value == null) {
+            return $this->setDraft();
+        }
+
+        return $this->setPublic();
+    }
+
+    public function setFeatured()
+    {
+        $this->status = 1;
+        $this->save();
+    }
+
+    public function setStandart()
+    {
+        $this->status = 0;
+        $this->save();
+    }
+
+    public function toggleFeatured($value)
+    {
+        if ($value == null) {
+            return $this->setStandart();
+        }
+
+        return $this->setFeatured();
+    }
+
+    public function setDateAttribute($value)
+    {
+        $date = Carbon::createFromFormat('d/m/y', $value)->format('Y-m-d');
+        $this->attributes['date'] = $date;
+
+    }
+
+    public function getDateAttribute($value)
+    {
+        $date = Carbon::createFromFormat('Y-m-d', $value)->format('d/m/y');
+        return $date;
+
+    }
+
+    public function getCategoryTitle()
+    {
+
+        return ($this->category != null)
+            ? $this->category->title
+            : 'без категории';
+    }
+    public function getCategoryID()
+    {
+        return $this->category != null ? $this->category->id : null;
+    }
+
+    public function getTagsTitles()
+    {
+        if (!$this->tags->isEmpty()) {
+            return implode(', ', $this->tags->pluck('title')->all());
+        }
+        return 'нет тэгов';
+    }
+
+}
