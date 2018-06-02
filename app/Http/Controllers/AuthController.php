e1dbab6fb256ce0fcafb3c73b81d209f10a59444
snarksliveshere <snarksliveshere@gmail.com>
2018-06-02 11:11:33 +0300

    аутентификация пользователя

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Auth;
+
+class AuthController extends Controller
+{
+    public function registerForm()
+    {
+        return view('pages.register');
+    }
+
+    public function register(Request $request)
+    {
+        $this->validate($request, ['name' => 'required', 'email' => 'required|email|unique:users', 'password' => 'required'
+        ]);
+
+        $user = User::add($request->all());
+        $user->generatePassword($request->get('password'));
+
+        return redirect('/login');
+    }
+
+    public function loginForm()
+    {
+        return view('pages.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email', 'password' => 'required'
+        ]);
+
+        // 1. проверить и залогинить на основе email & password
+        // 2. если неправильно выводим флэш
+        // 3. иначе редирект на главную
+
+        if(Auth::attempt([
+            'email' => $request->get('email'),
+            'password' => $request->get('password')
+        ])) {
+            return redirect('/');
+        }
+        // флэш :
+
+        return redirect()->back()->with('status', 'Неправильный логин или пароль');
+
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/login');
+    }
+}
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,8 +11,13 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $posts = Post::paginate(2);
+        if (\Auth::check()) {
+        } else {
 
+        }
+
+
+        $posts = Post::paginate(2);
 
         return view('pages.index')->with('posts', $posts);
 
