940bb7f33a2a21f29f8c7e8d0a89b1f2dae78033
snarksliveshere <snarksliveshere@gmail.com>
2018-06-22 13:59:39 +0300

    небольшие css фиксы. теперь надо перенести страницу продуктов

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -10,7 +10,7 @@ class PagesController extends Controller
 {
     public function index()
     {
-        $categories = ProductCategory::take(4)->get();
+        $categories = ProductCategory::take(3)->get();
         $posts = Post::where('status', 1)->take(4)->get();
         $products = Product::take(4)->get();
         return view('pages.index',['posts' => $posts, 'products' => $products, 'categories' => $categories]);
