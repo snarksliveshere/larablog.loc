a9309adef99cf8656ee14a71655d7849072538a1
snarksliveshere <snarksliveshere@gmail.com>
2018-07-18 15:22:06 +0300

    надпись на слайдерах

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -20,12 +20,12 @@
                                  data-appear="rotateInUpRight"
                                  data-delay="800">
                                 <h2 class="ltext-201 white p-t-19 p-b-43 respon1">
-                                    Решения любой сложности
+                                    TYCHO Corporation
                                 </h2>
                             </div>
                             <div class="layer-slick1 animated visible-false" data-appear="rotateIn" data-delay="1600">
                                 <a href="/about"
-                                   class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04"> TYCHO </a>
+                                   class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04"> Перейти </a>
                             </div>
                         </div>
                     </div>
@@ -35,18 +35,18 @@
                         <div class="flex-col-l-m h-full p-t-100 p-b-30 respon5">
                             <div class="layer-slick1 animated visible-false" data-appear="rollIn" data-delay="0">
 								<span class="ltext-101 white respon2">
-									Men New-Season
+									Скафандры для работ в открытом космосе
 								</span>
                             </div>
                             <div class="layer-slick1 animated visible-false"
                                     data-appear="lightSpeedIn"
                                     data-delay="800">
                                 <h2 class="ltext-201 white p-t-19 p-b-43 respon1">
-                                    Jackets & Coats
+                                    Скоро в продаже
                                 </h2>
                             </div>
                             <div class="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
-                                <a href="product.html"
+                                <a href="/posts/post/quae-unde-et-alias-placeat-excepturi-tempora"
                                         class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04"> Перейти </a>
                             </div>
                         </div>
